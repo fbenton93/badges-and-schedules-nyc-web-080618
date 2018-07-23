b71f7c3a9217cb08522dd21c99ce68c715419252
Forrest Benton <elliottbenton@gmail.com>
2018-07-23 22:47:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(speaker)
+  puts "Hello, my name is #{speaker}"
+end
+  
+def batch_badge_creator(speakers)
+  new_array = []
+  speakers.each do |speaker|
+    new_array << badge_maker(speaker)
+  end
+  new_array
+end
+  
